b0edeb83f6650eb20e5318b4799f11158b5a83fc
fineboy <fineboy0407@gmail.com>
2023-01-31 12:42:30 +0800

    simple strapi implementation'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from 'react';
+
+const parseJSON = (resp) => (resp.json? resp.json():resp);
+const checkStatus = (resp) => {
+  if(resp.status >= 200 && resp.status <300){
+    return resp;
+  }
+
+  return parseJSON(resp).then(resp=> {
+    throw resp;
+  });
+};
+
+const headers = {'Content-type':'application/json'};
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [error, setError] = useState(null);
+    const [restaurants, setRestaurants] = useState([]);
+
+    useEffect(()=>{
+      fetch('http://locallhost:1337/api/restaurants', {headers,method:'GET'})
+      .then(checkStatus).then(parseJSON).then(({data}) => setRestaurants(data)).catch((error)=>setError(error))
+    }, [])
+
+    if(error) {
+      return(
+        <div>An error occured: {error.message}</div>
+      );
+    }
+
+    return(
+      <div className='App'>
+        <ul>
+          {restaurants.map(({id, name}) => <li key={id}>{name}</li>)}
+        </ul>
+      </div>
+    )
 }
 
 export default App;
